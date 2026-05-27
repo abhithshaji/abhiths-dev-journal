ce7b531d634da5c0507a51f1ba4f0906bbfa139d
abhithshaji <abhithshaji@gmail.com>
2026-05-27 23:44:45 +0530

    feat : Select component

--- src/components/layout/LogsEntry.tsx
+++ src/components/layout/LogsEntry.tsx
@@ -4,6 +4,10 @@ import { ChevronDown, ChevronRight, ChevronUp } from "lucide-react";
 import { Heading4, Heading5 } from "@/components/heading";
 import {Input,Select} from "@/components/ui";
 
+
+const SERIES_OPTIONS = ["LC-75", "JS-30"];
+const STATUS_OPTIONS = ["New","Redo"];
+
 const LogsEntry = () => {
   const [isOpen, setIsOpen] = useState(true);
 
@@ -22,8 +26,17 @@ const LogsEntry = () => {
         <Heading4 title="DSA" />
       </div>
 
-      <div className="flex ml-10">
-        <div className="mr-4">
+      <div className="flex ml-10 items-center justify-between">
+        <div>
+          <Heading5 title="Problem ID" />
+          <Input
+            type="text"
+            id="dsa-problem-name"
+            name="dsa-problem-name"
+            onChange={function () {}}
+          />
+        </div>
+        <div>
           <Heading5 title="Problem Name" />
           <Input
             type="text"
@@ -34,7 +47,11 @@ const LogsEntry = () => {
         </div>
         <div>
           <Heading5 title="Series" />
-          <Select/>
+          <Select options={SERIES_OPTIONS}/>
+        </div>
+        <div>
+          <Heading5 title="Status" />
+          <Select options={STATUS_OPTIONS}/>
         </div>
       </div>
     </section>
--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,12 +1,49 @@
-"use client"
+"use client";
 import { ChevronDown } from "lucide-react";
 import { useState } from "react";
 
-export default function Select() {
+interface ISelectProps{
+  options : string[];
+}
+
+
+export default function Select({options}:ISelectProps) {
+  const [selecteedOption, setSelectedOption] = useState("");
+  const [openDropDown, setOpenDropDown] = useState(false);
+
+  const toggleDropDown = () => {
+    setOpenDropDown(!openDropDown);
+  };
+
+  const handleOptionSelection = (option: string) => {
+    toggleDropDown();
+    setSelectedOption(option);
+  };
 
   return (
-    <div className="">
-     
-    </div>
+    <section className="select-none">
+      <div
+        className="border rounded-lg px-4 py-1 w-28 cursor-pointer flex items-center justify-between"
+        onClick={toggleDropDown}
+      >
+        <p >{selecteedOption}</p>
+        <ChevronDown className="ml-2 " />
+      </div>
+      {openDropDown && (
+        <div className="text-center rounded-lg border mt-1 overflow-hidden">
+          {options.map((option, index) => {
+            return (
+              <p
+                key={option + index}
+                className="cursor-pointer hover:bg-foreground hover:text-background "
+                onClick={() => handleOptionSelection(option)}
+              >
+                {option}
+              </p>
+            );
+          })}
+        </div>
+      )}
+    </section>
   );
 }
